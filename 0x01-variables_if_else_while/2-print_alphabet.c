4b9176ba43d4363959d340db9868c79c880f51b4
Tosinkuzzy <Adeleye608@gmail.com>
2023-02-17 19:10:54 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
