7b1066726148d0d906274a8b8ad2e5d251af32f7
Tosinkuzzy <Adeleye608@gmail.com>
2023-02-17 19:36:11 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char low;
+
+for(low = 'a'; low <= 'z'; low++)
+putchar(low);
+for(low = 'A'; low  <= 'Z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
