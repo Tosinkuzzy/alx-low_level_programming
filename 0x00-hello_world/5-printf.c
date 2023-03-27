6f60fdd9bc6cac1a00c13d3a522a8552b038ae4c
Tosinkuzzy <Adeleye608@gmail.com>
2023-03-26 17:47:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,15 +6,15 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
+ * main - Prints "With proper grammar, but the outcome is
+ *                a piece of art, followed by a new line.
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	printf("%s",  "with proper grammar, but the outcome is a piece of art, \n");
+printf("%s", "With proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
