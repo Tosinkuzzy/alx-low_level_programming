30e7addbcc981b934ba57e668692dfcd2093a53d
Tosinkuzzy <Adeleye608@gmail.com>
2023-03-27 05:52:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,21 @@
-#include <unistd.h>
-/**
- *  main - Entry point
- *  Returns: Always 1 (success)
+/*
+ * File: 101-quote.c
+ * Auth: Tosin
  */
+
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful"- Dora korpar, 2015-10-19,
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+
 int main(void)
 {
-write(2,
-“and that piece of art is useful\" - Dora Korpar, 2015-10-19\n” ,59);
-	return(1);
+	write(2,
+			"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
