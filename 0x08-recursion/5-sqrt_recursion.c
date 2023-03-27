1f2fd0ca8c5fc87d3cec7cc08cb40e9c0e786670
tosinkuzzy <Adeleye608@gmail.com>
2023-03-28 00:10:46 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,17 +24,17 @@ int find_sqrt(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1;
-    return find_sqrt(n, 1);
+if (n < 0)
+return -1;
+return find_sqrt(n, 1);
 }
 
 int find_sqrt(int n, int i)
 {
-    if (i * i == n)
-        return i;
-    else if (i * i > n)
-        return -1;
-    else
-        return find_sqrt(n, i + 1);
+if (i * i == n)
+return i;
+else if (i * i > n)
+return -1;
+else
+return find_sqrt(n, i + 1);
 }
