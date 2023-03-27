10afd889d3ac919d75fa93019ee43824065b86c1
Tosinkuzzy <Adeleye608@gmail.com>
2023-03-26 17:36:00 -0700

    5-printf.c Upd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,20 @@
-#include <stdio>
+/*
+ * File: 5-printf.c
+ * Auth: Tosin
+ */
+
+#include <stdio.h>
+
 /**
- *  main - Entry point
- *  Return: Always 0 (success)
-*/
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-printf(“with proper grammar, but the outcome is a piece of art, \n" );
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
 	return (0);
 }
