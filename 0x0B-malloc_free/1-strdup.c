1661f42777f06a27c529b839e2248236b872e419
Tosinkuzzy <Adeleye608@gmail.com>
2023-03-27 21:00:25 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,14 @@
+/*
+ * File: 1-strdup.c
+ * Aith: Tosin
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*_strdup - duplicate to new memory space location   
-*@str: char
-*Return: 0
+* _strdup - duplicate to new memory space location
+* @str: char
+* Return: 0
 */
 char *_strdup(char *str)
 {
@@ -13,11 +18,13 @@ int n;
 
 if (str == NULL)
 return (NULL);
-for (n = 0; str[n] != '\0'; n++);
+for (n = 0; str[n] != '\0';
+n++);
 strnew = (char *)malloc(n + 1 * sizeof(char));
 if (strnew != NULL)
 {
-for (i = 0; str[i] != '\0'; i++)
+for (i = 0; str[i] != '\0';
+i++)
 strnew[i] = str[i];
 }
 else
