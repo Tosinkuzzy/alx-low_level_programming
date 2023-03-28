50a55caa83c42354b5dbd2b5a08dfea19f2aae92
Tosinkuzzy <Adeleye608@gmail.com>
2023-03-27 21:24:46 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,11 +1,16 @@
+/*
+ * File: 4-free_grid.c
+ * Auth: Tosin
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * free_grid - Entry point
  *@grid: rows of matrix
- *@height: columns of string
- * Return: a pointer to a 2 dimensional array of integers or null
+ *@height: height of the grid
+ * Return: Always 0.
  */
 void free_grid(int **grid, int height)
 {
