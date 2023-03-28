b334bc078a9f4495a3d28880bb8ad02d0b5f9205
tosinkuzzy <Adeleye608@gmail.com>
2023-03-28 01:38:10 +0100

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,52 @@
-/*
+/**
  * File: 100-is_palindrome.c
  * Auth: Tosin
  */
 
 #include "main.h"
-
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
-
 /**
- * find_strlen - returns the lenght of a string.
- * @s: The string to be measured.
+ * longitud - function that returns the power of number
+ * @s: string
  *
- * Return: The length of the string.
+ * Return: length of a string
  */
-int find_strlen(char *s)
+int longitud(char *s)
 {
-	int len = 0;
-if (*(s + len))
+int l = 0;
+
+if (*s != '\0')
 {
-	len++;
-	len += find_strlen(s + len);
+l++;
+return (longitud(s + 1) + l);
+}
+return (l);
+}
+/**
+ * comparar - function that returns the power of number
+ * @i: original number
+ * @l: counter to compare multiplication
+ * @s: string
+ * Return: square root 
+ */
+int comparar(int i, int l, char *s)
+{
+if (i >= l)
+return (1);
+else if (s[i] == s[l])
+return (comparar(i + 1, l - 1, s));
+else
+return (0);
+}
+/**
+ * is_palindrome - return 1 if number prime
+ * @s: number to evaluate
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+int l = longitud(s);
+int i = 0;
+
+return (comparar(i, l - 1, s));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,20 +15,20 @@
 int compare_strings(char* str1, char* str2)
 {
 
-	int i = 0;
+int i = 0;
 
-	    while (str1[i] != '\0' && str2[i] != '\0') {
+while (str1[i] != '\0' && str2[i] != '\0') {
 
-			if (str1[i] != str2[i]) {
+if (str1[i] != str2[i]) {
 
-					return 0;
+return 0;
 }
 
-				i++;
+i++;
 }
-		if (str1[i] != str2[i]) {
-
-			    return 0;
+if (str1[i] != str2[i])
+{
+return 0;
 }
-		    return 1;
+return 1;
 }
