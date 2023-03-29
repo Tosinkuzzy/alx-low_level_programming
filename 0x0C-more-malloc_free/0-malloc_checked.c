256515f6b89d518788192d4872f14e061b84851f
tosinkuzzy <Adeleye608@gmail.com>
2023-03-29 04:06:35 +0100

    3-array_range

--- 0x0C-more-malloc_free/0-malloc_checked.c
+++ 0x0C-more-malloc_free/0-malloc_checked.c
@@ -16,6 +16,7 @@ void *malloc_checked(unsigned int b)
 	void *i;
 
 	i = malloc(b);
+
 	if (i == NULL)
 		exit(98);
 	return (i);
--- 0x0C-more-malloc_free/main.h
+++ 0x0C-more-malloc_free/main.h
@@ -10,8 +10,10 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
+
 #include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -20,4 +22,4 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
-#endif/*MAIN_H*/
+#endif /*MAIN_H*/
