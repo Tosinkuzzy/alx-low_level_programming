89cfad520dd9622cf6d84afd1ade1f8167d15ade
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-03-29 02:56:12 +0100

    Update 101-mul.c

--- 0x0C-more-malloc_free/101-mul.c
+++ 0x0C-more-malloc_free/101-mul.c
@@ -1,105 +1,72 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-#define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
  *
- * Return: 0 if a non-digit is found,1 otherwise
+ * Return: void
  */
-int is_digit(char *s)
+void _print(char *str, int l)
 {
-int i = 0;
+	int i, j;
 
-while (s[i])
-{
-if (s[i] < '0' || s[i] > '9')
-return (0);
-i++;
-}
-return (1);
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	free(str);
 }
 
 /**
- * _strlen - returns the length of a string
- * @s: string to evaluate
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
  *
- * Return: the length of the string
+ * Return: pointer to dest, or NULL on failure
  */
-int _strlen(char *s)
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-int i = 0;
+	int j, k, mul, mulrem, add, addrem;
 
-while (s[i] != '\0')
-{
-i++;
-}
-return (i);
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
 }
 
 /**
- * errors - handles errors for main
- */
-void errors(void)
-{
-printf("Error\n");
-exit(98);
-}
-
-/**
- * main - mutiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
  *
- * Return: always 0 (success)
+ * Return: 0 if digits, 1 if not
  */
-int main(int argc, char *argv[])
+int check_for_digits(char **av)
 {
-char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
-s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-errors();
-len1 = _strlen(s1);
-len2 = _strlen(s2);
-len = len1 + len2 + 1;
-result = malloc (sizeof(int)*len);
-if (!result)
-return (1);
-for (i = 0; i <= len1 + len2; i++)
-result[i] = 0;
-for (len1 = len1 - 1; len1 >= 0;
-len1--)
-{
-digit1 = s1[len1] - '0';
-carry = 0;
-for (len2 = _strlen(s2) - 1;
-len2 >= 0; len2--)
-{
-digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
-result[len1 + len2 + 1] = carry % 10;
-carry /= 10;
-}
-if (carry > 0)
-result [len1 + len2 +1]
-+= carry;
-}
-for (i = 0; i < len - 1; i++)
-{
-if (result[i])
-a = 1;
-if (a)
-_putchar(result[i] + '0');
-}
-if (!a)
-_putchar('0');
-_putchar('\n');
-free(result);
-return (0);
-}
-
+	int i, j;
