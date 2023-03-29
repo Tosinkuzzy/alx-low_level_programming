226a71e1a1ee03e0be7f3542890f388976eb3296
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-03-29 03:00:28 +0100

    Update _putchar.c

--- 0x0C-more-malloc_free/_putchar.c
+++ 0x0C-more-malloc_free/_putchar.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <unistd.h>
 
 /**
@@ -6,7 +5,7 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * on error,-1 is returned, and errno is set approriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
