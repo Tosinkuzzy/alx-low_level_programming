b19bfa43008a67228ce2e4c89caedea76e4802e5
tosinkuzzy <Adeleye608@gmail.com>
2023-03-31 02:29:08 +0100

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+/*
+ * File: 0-print_list.c
+ * @G: Tosinkuzzy
+ */
+#include "lists.h"
+
+/**
+ * print_list - Entry point
+ * Description: Prints all the elements of list_t list
+ * @h: Pointer to head of the list
+ * Return: Number of nodes in list
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+
+else
+printf("[%d] %s\n", h->len, h->str);
+
+count++;
+h = h->next;
+}
+
+return (count);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+/*
+ * File: 4-free_list.c
+ * @G: Tosinkuzzy
+ */
+#include "lists.h"
+
+/**
+ * free_list - Entry point
+ * Description: Frees a linked list
+ * @head: A pointer to the head of the list
+ */
+
+void free_list(list_t *head)
+{
+list_t *current;
+
+while (head)
+{
+current = head->next;
+free(head->str);
+free(head);
+head = current;
+}
+}
