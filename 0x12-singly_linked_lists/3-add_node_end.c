50eb392ea476b37a100e438aad44ef114321449f
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-03-31 23:33:00 +0100

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,12 +5,12 @@
 #include "lists.h"
 
 /**
- *  * add_node_end - Entry point
- *   * Description: Adds new node at the end of a list_t list
- *    * @head: Pointer to pointer to the head node of the list
- *     * @str: String stored in the new node
- *      * Return: Address of the new element, or NULL if it failed
- *       */
+ * add_node_end - Entry point
+ * Description: Adds new node at the end of a list_t list
+ * @head: Pointer to pointer to the head node of the list
+ * @str: String stored in the new node
+ * Return: Address of the new element, or NULL if it failed
+ */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
