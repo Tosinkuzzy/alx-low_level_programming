1a3d88a53c177b4f74afdaa8ba1a5121be470a25
tosinkuzzy <Adeleye608@gmail.com>
2023-04-04 20:08:25 +0100

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ * Description: Frees a listint_t list.
+ * @head: Pointer to head node in the linked list
+ */
+void free_listint(listint_t *head)
+{
+listint_t *current;
+while (head)
+{
+current = head;
+head = head->next;
+free(current);
+}
+}
