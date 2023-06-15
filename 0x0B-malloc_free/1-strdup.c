d7c7ba2a5b4430d173a0c3f09deaf466c9e75449
tosinkuzzy <Adeleye608@gmail.com>
2023-06-16 00:03:02 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,9 +20,9 @@ if (str == NULL)
 return (NULL);
 {
 for (n = 0; str[n] != '\0';
-n++)
-strnew = (char *)malloc(n + 1 * sizeof(char));
+n++);
 }
+strnew = (char *)malloc(n + 1 * sizeof(char));
 if (strnew != NULL)
 {
 for (i = 0; str[i] != '\0';
