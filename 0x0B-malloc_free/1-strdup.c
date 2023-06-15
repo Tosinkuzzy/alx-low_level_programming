10fb81caba3a18bf5a8bb11c0ad0153de6f16e1a
tosinkuzzy <Adeleye608@gmail.com>
2023-06-16 00:13:58 +0100

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,8 +19,7 @@ int n;
 if (str == NULL)
 return (NULL);
 {
-for (n = 0; str[n] != '\0';
-n++);
+for (n = 0; str[n] != '\0'; n++);
 }
 strnew = (char *)malloc(n + 1 * sizeof(char));
 if (strnew != NULL)
