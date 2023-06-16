f0c1e85425a80ea0ca3775b002c968fa76aeec1e
tosinkuzzy <Adeleye608@gmail.com>
2023-06-16 06:18:07 +0100

    101-mul.c

--- 0x0C-more-malloc_free/101-mul.c
+++ 0x0C-more-malloc_free/101-mul.c
@@ -1,5 +1,9 @@
-#include "main.h"
-
+/*
+ * File: 101-mul.c
+ * Author: Tosinkuzzy
+ */
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * _print - moves a string one place to the left and prints the string
  * @str: string to move
@@ -7,66 +11,26 @@
  *
  * Return: void
  */
-void _print(char *str, int l)
+int main(int argc, char *argv[])
 {
-	int i, j;
+int num1, num2, sum;
 
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-
-	_putchar('\n');
-	free(str);
+if (argc != 3)
+{
+printf("Error\n");
+return (98);
 }
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
 
-/**
- * mul - multiplies a char with a string and places the answer into dest
- * @n: char to multiply
- * @num: string to multiply
- * @num_index: last non NULL index of num
- * @dest: destination of multiplication
- * @dest_index: highest index to start addition
- *
- * Return: pointer to dest, or NULL on failure
- */
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+if (num1 == 0 || num2 == 0)
 {
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
+printf("Error\n");
+return (98);
 }
+sum = num1 * num2;
 
-/**
- * check_for_digits - checks the arguments to ensure they are digits
- * @av: pointer to arguments
- *
- * Return: 0 if digits, 1 if not
- */
-int check_for_digits(char **av)
-{
-	int i, j;
+printf("%d\n", sum);
+
+return (0);
+}
