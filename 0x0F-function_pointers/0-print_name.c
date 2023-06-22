c52e226128e9e43960ac6380643962b416dad029
tosinkuzzy <Adeleye608@gmail.com>
2023-06-22 03:42:45 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
 /**
-* print_name - print name using pointer to function
-* @name: string to add
-* @f: pointer to function
-*/
+ * print_name - Entry
+ * des: Print a name
+ * @name: print name
+ * @f: A function that takes a character pointer argument and return void.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
-	{
 		return;
-	}
 	f(name);
 }
