52595acb6089ed8595de6c6ea227d727118ce257
tosinkuzzy <Adeleye608@gmail.com>
2023-06-25 21:11:11 +0100

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,56 +1,25 @@
-/**
+/*
  * File: 100-is_palindrome.c
- * Auth: Tosin
+ * Author: Tosinkuzzy
  */
 
 #include "main.h"
+#include <string.h>
 /**
- * longitud - function that returns the power of number
- * @s: string
+ * is_palindrome - Entry p
+ * @s: string to check if it is a palindrome
  *
- * Return: length of a string
- */
-int longitud(char *s)
-{
-int l = 0;
-
-if (*s != '\0')
-{
-l++;
-return (longitud(s + 1) + l);
-}
-return (l);
-}
-/**
- * comparar - function that returns the power of number
- * @i: original number
- * @l: counter to compare multiplication
- * @s: string
- * Return: square root
- */
-int comparar(int i, int l, char *s)
-{
-if (i >= l)
-{
-return (1);
-}
-else if (s[i] == s[l])
-{
-return (comparar(i + 1, l - 1, s));
-}
-else
-return (0);
-}
-/**
- * is_palindrome - return 1 if number prime
- * @s: number to evaluate
- *
- * Return: 1 or 0
+ * Return: 1 if s is a palindrome, 0 otherwise.
  */
 int is_palindrome(char *s)
 {
-int l = longitud(s);
-int i = 0;
+	int len = strlen(s);
+	int i;
 
-return (comparar(i, l - 1, s));
+	for (i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - i - 1])
+			return (0);
+	}
+	return (1);
 }
