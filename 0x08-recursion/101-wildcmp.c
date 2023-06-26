d2b1487475ec422411f70e5758b8c3d09b831538
tosinkuzzy <Adeleye608@gmail.com>
2023-06-26 01:05:11 +0100

    101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,15 +19,21 @@ if (*s2 == '*')
 {
 if (*(s2 + 1) == '\0')
 {
-	return (1);
+return (1);
 }
-while (*s1 != '\0' && !wildcmp(s1 + 1, s2) && !wildcmp(s1, s2 + 1))
-s1++;
-return (*s1 != '\0');
-}
-if (*s1 == '\0' || *s2 == '\0')
+if (*s1 == '\0')
 {
-return (*s1 == *s2);
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+}
+if (wildcmp(s1 + 1, s2))
+{
+return (1);
+}
+return (wildcmp(s1, s2 + 1));
+}
+if (*s1 == '\0')
+{
+return (*s2 == '\0');
 }
 if (*s1 == *s2)
 {
