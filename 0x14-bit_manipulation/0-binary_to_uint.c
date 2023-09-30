7c9aaaf333d6643703b33e8981fd48272ce9b653
Tosinkuzzy/alx-pre_course <Adeleye608@gmail.com>
2023-09-30 03:08:05 +0100

    0-binary_to_uint updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/*
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
+ */
 unsigned int binary_to_uint(const char *b)
 {
 int n;
