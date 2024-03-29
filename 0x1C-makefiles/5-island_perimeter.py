df7768255a4bdd036b8d90d90a200160740fff38
Tosinkuzzy <Adeleye608@gmail.com>
2024-03-29 04:34:18 +0100

    insland perimeter file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines an island perimeter measuring function.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    pass
